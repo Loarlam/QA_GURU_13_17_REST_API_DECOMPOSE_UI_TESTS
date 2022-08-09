f3ebf97f970d4430351d19121d0a402d3b0106e2
Loarlam <Loarlam@ya.ru>
2022-08-09 18:06:50 +0300

    Совместил тесты в один класс.

--- src/test/java/guru/qa/tests/DemoWebShopUIandAPITests.java
+++ src/test/java/guru/qa/tests/DemoWebShopUIandAPITests.java
@@ -1,7 +1,6 @@
 package guru.qa.tests;
 
 import io.qameta.allure.*;
-import io.qameta.allure.internal.shadowed.jackson.databind.ser.Serializers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,31 @@ import org.junit.jupiter.api.Test;
 import static guru.qa.helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 
-public class DemoWebShopRegAndChangedAPIUI extends BaseTest {
+public class DemoWebShopUIandAPITests extends BaseTest {
+    @Test
+    @Tag("demoWebShop1")
+    @Owner("Loarlam")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Участие Jenkins в процессе регистрации на сайте (UI)")
+    @Story("Jenkins тянет код автотеста регистрации на сайте из гита, при отработке которого формируется отчёт в Allure Report")
+    @Description("Методика запуска UI процесса регистрации на сайте посредством подтягивания кода из github в Jenkins, с выводом отчёта Allure, " +
+            "позволяет решить проблему привязки к локальной машине")
+    @DisplayName("Регистрация на сайте demowebshop.tricentis.com с последующей проверкой результата регистрации (UI)")
+    void registeringUI() {
+        dataForTheTest = new DataForTheTest();
+
+        pageOfRegistrationForm.openingMinimalContentInSite()
+                .openingWebsiteRegisterPage()
+                .settingFirstName(dataForTheTest.FIRST_NAME_FOR_REGISTRATION)
+                .settingSurname(dataForTheTest.LAST_NAME_FOR_REGISTRATION)
+                .settingEmail(dataForTheTest.EMAIL_FOR_REGISTRATION)
+                .settingPassword(dataForTheTest.PASSWORD_FOR_REGISTRATION)
+                .settingConfirmPassword(dataForTheTest.PASSWORD_FOR_REGISTRATION)
+                .clickingOnRegisterButton()
+                .checkingResultOfRegistration(dataForTheTest.resultOfRegistration)
+                .clickingOnLogoutButton();
+    }
+
     @Test
     @Tag("demoWebShop2")
     @Owner("Loarlam")
@@ -21,7 +44,7 @@ public class DemoWebShopRegAndChangedAPIUI extends BaseTest {
             "с выводом отчёта Allure, позволяет решить проблему привязки к локальной машине (API)")
     @Step("Прохождение регистрации на сайте")
     @DisplayName("Регистрация, авторизация и смена данных пользователя на сайте demowebshop.tricentis.com с последующей проверкой результата регистрации (API)")
-    void registrationAndAuthOnSiteWithCheckingResult() {
+    void registeringAndChangedAPIandUI() {
         dataForTheTest = new DataForTheTest();
 
         given()
